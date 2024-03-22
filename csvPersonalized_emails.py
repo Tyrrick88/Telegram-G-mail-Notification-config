b4a2ed2ca9be854086c69bd9fe91670cba77ee9d
Tyrrick Ndung'u Ngunyi <123152274+Tyrrick88@users.noreply.github.com>
2024-03-22 19:36:00 +0300

    ADDITION : Sending personalized messages form a csv files to multiple users.

---
+++ csvPersonalized_emails.py
@@ -0,0 +1,20 @@
+import csv, smtplib, ssl
+
+message = """Subject: Your grade
+
+Hi {name}, your grade is {grade}"""
+from_address = "my@gmail.com"
+password = input("Type your password and press enter: ")
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(from_address, password)
+    with open("contacts_file.csv") as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip header row
+        for name, email, grade in reader:
+            server.sendmail(
+                from_address,
+                email,
+                message.format(name=name,grade=grade),
+            )
