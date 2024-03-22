e6ec98cd955376a0c4b5edb11568c650567f5968
Tyrrick Ndung'u Ngunyi <123152274+Tyrrick88@users.noreply.github.com>
2024-03-22 19:29:59 +0300

    ADDITION : securing the smtp server using the starttls encryption module

--- sslgmailConfig.py
+++ sslgmailConfig.py
@@ -1,11 +1,22 @@
 import smtplib, ssl
 
-port = 465
-password = input("Please Enter your Gmail password: ")
 
-# Create a context 
+port = 465
+smtp_server = "smtp.gmail.com"
+sender_email = input("Enter the senders email addreess: ")
+reciever_email = input("Enter the receivers email address: ")
+password = input("Enter the senders email address password: ")
+message = """\
+Subject : Python plain e-mail attachment
+
+amkaa mzee
+"""
+
 
 context = ssl.create_default_context()
-
-with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
-    server.login = ("Your Email address", password)
+with smtplib.SSL(smtp_server, port)as server:
+    server.ehlo()
+    server.starttls(context=context)
+    server.login(sender_email, password)
+    server.sendmail(sender_email, reciever_email, message )
+    server.quit()
